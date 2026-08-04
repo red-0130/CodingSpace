06660df4aab1b311990f977dfefc9e50e351eb90
red-0130 <100506096+red-0130@users.noreply.github.com>
2026-08-04 01:29:25 -0400

    build: remove "coder-login" module
    Module does not do what I expected it to do so it is removed

--- modules.tf
+++ modules.tf
@@ -23,10 +23,3 @@ module "code-server" {
     "chat.disableAIFeatures"           = true,
   }
 }
-
-module "coder-login" {
-  count    = data.coder_workspace.me.start_count
-  source   = "registry.coder.com/coder/coder-login/coder"
-  version  = "1.1.1"
-  agent_id = coder_agent.main.id
-}
